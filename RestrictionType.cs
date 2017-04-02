3e0d369a6929fc5f611e2361b1daa9d25e8c3efb
KPKanimator <k.korobchinskiy@khai.edu>
2017-04-02 22:51:05 +0300

    Вынес в отдельный файл RestrictionType типы ограничений

---
+++ RestrictionType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hs071_cs
+{
+  // Классы задач 
+  public enum Restriction
+  {
+    Power /*степенные ограничения*/,
+    Lineal /*линейные ограничения*/,
+    AlphaPower,
+    FixedRadius
+  }
+  public enum CircleTaskTypes
+  {
+    FixedRadius,
+    DynamicRadius
+  }
+}
